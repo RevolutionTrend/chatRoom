362c09f63fc5a05f7439c6f1e5ce852a294fb949
LEI YUAN <Yuand2010@hotmail.com>
2018-07-31 14:29:32 +0800

    encode html

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
-$(document).ready(function () {
+const host = window.location.host;
+const socket = window.io('http://' + host);
 
-    const socket = window.io('http://192.168.38.126:4040');
+$(document).ready(function () {
 
     const doLogin = username => {
         socket.emit('login', {
@@ -9,6 +10,15 @@ $(document).ready(function () {
         document.getElementById('login').style.display = 'none';
         document.getElementsByClassName('typein')[0].removeAttribute('disabled');
         document.getElementsByClassName('typeBtn')[0].removeAttribute('disabled');
+    }
+
+    const encodeHtml = str => {
+        let temp = str.replace(/&/g, '&amp;');
+        temp = temp.replace(/\</g, '&lt;');
+        temp = temp.replace(/\>/g, '&gt;');
+        temp = temp.replace(/\"/g, '&quot;');
+        temp = temp.replace(/\\n/g, '<br />');
+        return temp;
     }
 
     $('.loginBtn').on('click', function () {
@@ -55,21 +65,21 @@ $(document).ready(function () {
 
     socket.on('login', function (obj) {
         const name = obj.user.username;
-        $('#chat>tbody').append('<tr><td colspan="3" align="center" height="44"><span class="usrInOut">' + name + ' 加入了聊天室</span></td></tr>');
+        $('#chat>tbody').append('<tr><td colspan="3" align="center" height="44"><span class="usrInOut">' + encodeHtml(name) + ' 加入了聊天室</span></td></tr>');
     });
 
     socket.on('logout', function (obj) {
         const name = obj.user.username;
-        $('#chat>tbody').append('<tr><td colspan="3" align="center" height="44"><span class="usrInOut">' + name + ' 离开了聊天室</span></td></tr>');
+        $('#chat>tbody').append('<tr><td colspan="3" align="center" height="44"><span class="usrInOut">' + encodeHtml(name) + ' 离开了聊天室</span></td></tr>');
     });
 
     socket.on('message', function (obj) {
         if (obj.id === socket.id) {
-            $('#chat>tbody').append('<tr><td></td><td align="right"><span class="username">' + obj.username + '</span></td><td rowspan="2" valign="top" width="48"><img class="userIcon" src="assets/person.png" /></td></tr>');
-            $('#chat>tbody').append('<tr><td></td><td align="right"><div class="message message-right">' + obj.msg + '</div></td></tr>');
+            $('#chat>tbody').append('<tr><td></td><td align="right"><span class="username">' + encodeHtml(obj.username) + '</span></td><td rowspan="2" valign="top" width="48"><img class="userIcon" src="assets/person.png" /></td></tr>');
+            $('#chat>tbody').append('<tr><td></td><td align="right"><div class="message message-right">' + encodeHtml(obj.msg) + '</div></td></tr>');
         } else {
-            $('#chat>tbody').append('<tr><td rowspan="2" valign="top" width="48"><img class="userIcon" src="assets/person.png" /></td><td><span class="username">' + obj.username + '</span></td><td></td></tr>');
-            $('#chat>tbody').append('<tr><td><div class="message message-left">' + obj.msg + '</div></td><td></td></tr>');
+            $('#chat>tbody').append('<tr><td rowspan="2" valign="top" width="48"><img class="userIcon" src="assets/person.png" /></td><td><span class="username">' + encodeHtml(obj.username) + '</span></td><td></td></tr>');
+            $('#chat>tbody').append('<tr><td><div class="message message-left">' + encodeHtml(obj.msg) + '</div></td><td></td></tr>');
         }
     });
 });
